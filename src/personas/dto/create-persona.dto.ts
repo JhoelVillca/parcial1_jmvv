d191d70c0a75d81e199ed7e6608cccab86e23009
JhoelVillca <jhoel.mv.villca@gmail.com>
2026-04-09 20:14:21 -0400

    primer parcial

--- src/personas/dto/create-persona.dto.ts
+++ src/personas/dto/create-persona.dto.ts
@@ -1,1 +1,7 @@
-export class CreatePersonaDto {}
+export class CreatePersonaDto {
+  ci: string | undefined;
+  nombres: string | undefined;
+  primer_apellido: string | undefined;
+  segundo_apellido: string | undefined;
+  fecha_nacimiento: Date | undefined;
+}
